fe0a4dfe2f76bb28a104ca62eed7829de76387b9
Fatal-X <90718956+Fatal-X@users.noreply.github.com>
2021-09-16 15:09:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const main = document.getElementById('main');
 let parts = [];
 
 let images = [
-  "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-  "https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-  "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80"
+  "IMG_2296.jpg",
+  "IMG_2305.jpg",
+  "IMG_2326.jpg"
 ];
 let current = 0;
 let playing = false;
